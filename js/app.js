bc5ebf28a6fe33faf3378b52b071c6d6620d5d72
Daisho <daishokomiyama@gmail.com>
2018-08-12 11:48:10 -0400

    Installing Angular

--- js/app.js
+++ js/app.js
@@ -1,25 +1,71 @@
 'use strict';
 
-var textarea = document.getElementById('textArea');
-var encodeBtn = document.getElementById('encode');
-var decodeBtn = document.getElementById('decode');
-var title = document.getElementById('title');
-var url = document.getElementById('url');
-var obj = {};
+var linkListApp = angular.module('linkListApp', []);
 
-encodeBtn.addEventListener('click', function() {
-    var a = title.value;
-    console.log(a);
-    var b = btoa(a);
+linkListApp.controller('LinkListController', function LinkListController($scope) {
+    $scope.forms = [
+        {
+            label: {
+                name: 'Link',
+                title: 'Title',
+                url: 'URL',
+                output: 'Output'
+            },
+            title: '',
+            url: ''
+        }
+    ];
 
-    console.log(b);
-    textarea.innerHTML = b;
+    $scope.textarea = '';
+
+    $scope.encode = function() {
+        var obj = {};
+        for (let i = 0; i < $scope.forms.length; i++) {
+            console.log($scope.forms[i].title);
+            obj.t = $scope.forms[i].title;
+            obj.u = $scope.forms[i].url;
+            console.log(obj);
+        }
+        obj = JSON.stringify(obj);
+        console.log(obj);
+        var encoded = btoa(obj);
+        $scope.textarea = encoded;
+    }
+
+    $scope.decode = function() {
+        console.log($scope.textarea);
+
+        const text = atob($scope.textarea);
+        console.log(text);
+        var u = JSON.stringify(text);
+        console.log(u);
+    }
 });
 
-decodeBtn.addEventListener('click', function() {
-    var a = textarea.value;
-    console.log('encoded: ' + a);
-    var b = atob(a);
-    console.log('decoded: ' + b);
-    textarea.innerHTML = b;
-});
+// var textarea = document.getElementById('textArea');
+// var encodeBtn = document.getElementById('encode');
+// var decodeBtn = document.getElementById('decode');
+// var title = document.getElementById('title');
+// var url = document.getElementById('url');
+// var obj = {};
+
+// encodeBtn.addEventListener('click', function() {
+//     obj.a = title.value;
+//     console.log(obj);
+//     obj = JSON.stringify(obj);
+//     console.log(obj);
+//     var b = btoa(obj);
+
+//     // console.log(b);
+//     textarea.innerHTML = b;
+// });
+
+// decodeBtn.addEventListener('click', function() {
+//     var k = textarea.value;
+//     console.log('encoded: ' + k);
+//     var b = atob(k);
+//     b = JSON.stringify(b);
+//     alert(b);
+//     console.log('decoded2: ' + b.a);
+//     textarea.innerHTML = b;
+// });
